a15f31931c63965c16e83cc1b52c3f37318d9cec
Jonathan Bailey <bailey.jonathan@magiccityit.com>
2024-08-27 11:59:07 -0500

    initial commit

---
+++ src/pipeline_cli.py
@@ -0,0 +1,83 @@
+import argparse
+from textwrap import dedent
+
+from src.data_utils import get_current_season_year
+from src.dataset_runner import pbp_dataset
+
+META_VAR = "YYYY"
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Import Data from nfl-data-py to a ClearML Dataset",
+        epilog=dedent(
+            """\
+            FURTHER READING
+            ---------------
+
+            Subject                             Link
+            -------                             ----
+            Field Descriptions:                 https://www.nflfastr.com/articles/field_descriptions.html
+            WP Calibration Data:                https://github.com/guga31bb/metrics/tree/master/wp_tuning
+        """
+        ),
+    )
+
+    subparsers = parser.add_subparsers(help="subcommand help", required=True)
+
+    _add_dataset_subparser(subparsers)
+
+    kwargs = vars(parser.parse_args())
+
+    subcommand = kwargs.pop("backend")
+    del kwargs["/subparser1"]
+
+    subcommand(**kwargs)
+
+
+def _add_dataset_subparser(subparsers):
+
+    subparser = subparsers.add_parser("datasets", description="Import data to a ClearML Dataset")
+
+    subparser.add_argument(
+        "-s", "--start-year", type=int, metavar=META_VAR, required=False, default=1999, help="Starting Season.  Default: 1999"
+    )
+
+    subparser.add_argument(
+        "-e",
+        "--end-year",
+        type=int,
+        metavar="YYYY",
+        required=False,
+        default=get_current_season_year(),
+        help=f"Ending Season.  Default: {get_current_season_year()}",
+    )
+
+    subparser.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        metavar="DATASET_NAME",
+        required=False,
+        default="NFL Play-by-Play Data",
+        help="Dataset Name.  Default: NFL Play-by-Play Data",
+    )
+
+    subparser.add_argument("-u", "--update", action="store_true", help="Update the dataset with the latest data")
+
+    subparser.add_argument(
+        "-p",
+        "--project",
+        type=str,
+        metavar="PROJECT_NAME",
+        required=False,
+        default="NFL Models",
+        help="Project Name.  Default: NFL Models",
+    )
+
+    subparser.add_argument("-c", "--calibrate", action="store_true", help="Generate calibration data")
+
+    subparser.add_argument("--vegas", action="store_true", help="Generate Vegas WP Calibration Data")
+
+    subparser.set_defaults(backend=pbp_dataset)
---
+++ tests/unit/test_dataset_runner.py
@@ -0,0 +1,71 @@
+import unittest
+from types import SimpleNamespace
+from unittest.mock import MagicMock, call, patch
+
+import pandas as pd
+
+from src.dataset_runner import pbp_dataset
+
+
+class DatasetRunner(unittest.TestCase):
+
+    def setUp(self):
+        self.seasons = [2018, 2019, 2020]
+        self.test_data = pd.DataFrame({"column1": [1, 2, 3], "column2": ["a", "b", "c"], "season": self.seasons})
+        self.grouped_test_data = self.test_data.groupby("season")
+        self.args = SimpleNamespace(
+            **{
+                "start_year": 2018,
+                "end_year": 2020,
+                "dataset": "NFL Play-by-Play Data",
+                "project": "NFL WP Model",
+                "update": True,
+                "calibrate": True,
+                "vegas": True,
+            }
+        )
+
+    @patch("src.dataset_runner._calibrate_data")
+    @patch("src.dataset_runner.update_dataset")
+    @patch("src.dataset_runner.process_dataframe")
+    @patch("src.dataset_runner.clean_nfl_data")
+    @patch("src.dataset_runner.load_pbp_data")
+    @patch("src.dataset_runner.print")
+    @patch("src.dataset_runner.os.makedirs")
+    @patch("src.dataset_runner.get_seasons_to_import")
+    @patch("src.dataset_runner.generate_tags")
+    @patch("src.dataset_runner.get_dataset")
+    def test_pbp_dataset(
+        self,
+        mock_get_dataset,
+        mock_generate_tags,
+        mock_get_seasons_to_import,
+        mock_makedirs,
+        mock_print,
+        mock_load_pbp_data,
+        mock_clean_nfl_data,
+        mock_process_dataframe,
+        mock_update_dataset,
+        mock_calibrate_data,
+    ):
+        mock_get_dataset.return_value = MagicMock()
+        mock_generate_tags.return_value = ["2018-2020", "COMPLETE"]
+        mock_get_seasons_to_import.return_value = self.seasons
+        mock_load_pbp_data.return_value = self.test_data
+        mock_calibrate_data.return_value = self.test_data
+        pbp_dataset(self.args)
+
+        mock_makedirs.assert_called_with("pbp_data", exist_ok=True)
+        mock_print.assert_has_calls(
+            [
+                call("Importing Play-by-Play Data for the following years:\n[2018, 2019, 2020]"),
+            ]
+        )
+        mock_load_pbp_data.assert_called_with(self.seasons)
+        mock_clean_nfl_data.assert_called_with(self.test_data)
+        mock_process_dataframe.assert_called()
+        mock_update_dataset.assert_called_with(mock_get_dataset.return_value, "pbp_data")
+
+
+if __name__ == "__main__":
+    unittest.main()
