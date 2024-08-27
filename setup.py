aba53fea2c6a46594b9645cf3ca4dbf295a99984
Jonathan Bailey <bailey.jonathan@magiccityit.com>
2024-08-27 13:15:30 -0500

    fix tests

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ dev_requirements = ["pre-commit", "black", "flake8", "isort", "pytest"]
 
 setup(
     name="pipeline-cli",
-    version="0.2.0",
+    version="0.3.0",
     author="Jonathan Bailey",
     author_email="jonathan@jelbailey.com",
     description="A CLI tool for managing ClearML Pipelines",
